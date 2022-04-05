37e4ae1ca89cec16847d4196e72554ccc6d0cf61
Alex <lagotrixa@gmail.com>
2022-04-06 00:07:53 +0300

    init

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,163 +1,77 @@
-//! This contract implements simple counter backed by storage on blockchain.
-//!
-//! The contract provides methods to [increment] / [decrement] counter and
-//! [get it's current value][get_num] or [reset].
-//!
-//! [increment]: struct.Counter.html#method.increment
-//! [decrement]: struct.Counter.html#method.decrement
-//! [get_num]: struct.Counter.html#method.get_num
-//! [reset]: struct.Counter.html#method.reset
-
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen};
+use near_sdk::{env, near_bindgen,Promise};
 
-near_sdk::setup_alloc!();
+// 1 NEAR
+const AMOUNT: u128 = 1_000_000_000_000_000_000_000_000;
 
-// add the following attributes to prepare your code for serialization and invocation on the blockchain
-// More built-in Rust attributes here: https://doc.rust-lang.org/reference/attributes.html#built-in-attributes-index
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Counter {
-    // See more data types at https://doc.rust-lang.org/book/ch03-02-data-types.html
-    val: i8, // i8 is signed. unsigned integers are also available: u8, u16, u32, u64, u128
+pub struct Contract {
+    // SETUP CONTRACT STATE
+    key: String,
 }
 
 #[near_bindgen]
-impl Counter {
-    /// Returns 8-bit signed integer of the counter value.
-    ///
-    /// This must match the type from our struct's 'val' defined above.
-    ///
-    /// Note, the parameter is `&self` (without being mutable) meaning it doesn't modify state.
-    /// In the frontend (/src/main.js) this is added to the "viewMethods" array
-    /// using near-cli we can call this by:
-    ///
-    /// ```bash
-    /// near view counter.YOU.testnet get_num
-    /// ```
-    pub fn get_num(&self) -> i8 {
-        return self.val;
+impl Contract {
+    // INITIALIZE CONTRACT, PREVENTS CHANGES TO THE CONTRACT
+    #[init]
+    pub fn wallet(_key: String) -> Self {
+            Self{key: _key}
     }
 
-    /// Increment the counter.
-    ///
-    /// Note, the parameter is "&mut self" as this function modifies state.
-    /// In the frontend (/src/main.js) this is added to the "changeMethods" array
-    /// using near-cli we can call this by:
-    ///
-    /// ```bash
-    /// near call counter.YOU.testnet increment --accountId donation.YOU.testnet
-    /// ```
-    pub fn increment(&mut self) {
-        // note: adding one like this is an easy way to accidentally overflow
-        // real smart contracts will want to have safety checks
-        // e.g. self.val = i8::wrapping_add(self.val, 1);
-        // https://doc.rust-lang.org/std/primitive.i8.html#method.wrapping_add
-        self.val += 1;
-        let log_message = format!("Increased number to {}", self.val);
-        env::log(log_message.as_bytes());
-        after_counter_change();
+    // ADD CONTRACT METHODS HERE
+    #[payable]
+    pub fn get_money(&mut self, _key: String) -> bool {
+        let hashed_input = env::sha256(_key.as_bytes());
+        let hashed_input_hex = hex::encode(&hashed_input);
+
+        if hashed_input_hex == self.key {
+            Promise::new(env::predecessor_account_id()).transfer(AMOUNT);
+            env::log_str("Key is correct. Paid!");
+            return true;
+        } else {
+            env::log_str("Key is wrong!");
+            return false;
+        }
     }
 
-    /// Decrement (subtract from) the counter.
-    ///
-    /// In (/src/main.js) this is also added to the "changeMethods" array
-    /// using near-cli we can call this by:
-    ///
-    /// ```bash
-    /// near call counter.YOU.testnet decrement --accountId donation.YOU.testnet
-    /// ```
-    pub fn decrement(&mut self) {
-        // note: subtracting one like this is an easy way to accidentally overflow
-        // real smart contracts will want to have safety checks
-        // e.g. self.val = i8::wrapping_sub(self.val, 1);
-        // https://doc.rust-lang.org/std/primitive.i8.html#method.wrapping_sub
-        self.val -= 1;
-        let log_message = format!("Decreased number to {}", self.val);
-        env::log(log_message.as_bytes());
-        after_counter_change();
-    }
-
-    /// Reset to zero.
-    pub fn reset(&mut self) {
-        self.val = 0;
-        // Another way to log is to cast a string into bytes, hence "b" below:
-        env::log(b"Reset counter to zero");
-    }
-}
-
-// unlike the struct's functions above, this function cannot use attributes #[derive(…)] or #[near_bindgen]
-// any attempts will throw helpful warnings upon 'cargo build'
-// while this function cannot be invoked directly on the blockchain, it can be called from an invoked function
-fn after_counter_change() {
-    // show helpful warning that i8 (8-bit signed integer) will overflow above 127 or below -128
-    env::log("Make sure you don't overflow, my friend.".as_bytes());
 }
 
 /*
  * the rest of this file sets up unit tests
  * to run these, the command will be:
- * cargo test --package rust-counter-tutorial -- --nocapture
- * Note: 'rust-counter-tutorial' comes from cargo.toml's 'name' key
+ * cargo test --package rust-template -- --nocapture
+ * Note: 'rust-template' comes from Cargo.toml's 'name' key
  */
 
 // use the attribute below for unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::MockedBlockchain;
-    use near_sdk::testing_env;
-    use near_sdk::test_utils::VMContextBuilder;
-    use near_sdk::json_types::ValidAccountId;
-    use near_sdk::serde::export::TryFrom;
-
-    // simple helper function to take a string literal and return a ValidAccountId
-    fn to_valid_account(account: &str) -> ValidAccountId {
-        ValidAccountId::try_from(account.to_string()).expect("Invalid account")
-    }
+    use near_sdk::test_utils::{get_logs, VMContextBuilder};
+    use near_sdk::{testing_env, AccountId};
 
     // part of writing unit tests is setting up a mock context
     // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: ValidAccountId) -> VMContextBuilder {
+    fn get_context(predecessor: AccountId) -> VMContextBuilder {
         let mut builder = VMContextBuilder::new();
         builder.predecessor_account_id(predecessor);
         builder
     }
 
-    // mark individual unit tests with #[test] for them to be registered and fired
+    // TESTS HERE
     #[test]
-    fn increment() {
-        // set up the mock context into the testing environment
-        let context = get_context(to_valid_account("foo.near"));
-        testing_env!(context.build());
-        // instantiate a contract variable with the counter at zero
-        let mut contract = Counter { val: 0 };
-        contract.increment();
-        println!("Value after increment: {}", contract.get_num());
-        // confirm that we received 1 when calling get_num
-        assert_eq!(1, contract.get_num());
-    }
-
-    #[test]
-    fn decrement() {
+    fn check_get_money() {
         let context = VMContextBuilder::new();
         testing_env!(context.build());
         let mut contract = Counter { val: 0 };
-        contract.decrement();
-        println!("Value after decrement: {}", contract.get_num());
-        // confirm that we received -1 when calling get_num
-        assert_eq!(-1, contract.get_num());
-    }
 
-    #[test]
-    fn increment_and_reset() {
-        let context = VMContextBuilder::new();
-        testing_env!(context.build());
-        let mut contract = Counter { val: 0 };
-        contract.increment();
-        contract.reset();
-        println!("Value after reset: {}", contract.get_num());
-        // confirm that we received -1 when calling get_num
-        assert_eq!(0, contract.get_num());
+        let mut guess_result = contract.get_money("WrongKey".to_string());
+        assert!(!(guess_result), "Expected a failure from the WrongKey");
+        assert_eq!(get_logs(), ["Key is wrong!"], "Expected a failure log.");
+
+        guess_result = contract.get_money("CorrectKey".to_string());
+        assert!(guess_result, "Expected the CorrectKey to return true.");
+        assert_eq!(get_logs(), ["Key is correct. Paid!"], "Expected a successful log.");
     }
 }
